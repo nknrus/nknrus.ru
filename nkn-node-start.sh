6342f2fc4d04088caecfe688213c8ac0e309314f
nknrus <86024916+nknrus@users.noreply.github.com>
2021-08-26 01:17:51 +0600

    Update nkn-node-start.sh

--- nkn-node-start.sh
+++ nkn-node-start.sh
@@ -5,14 +5,11 @@ apt -y upgrade
 apt -y install unzip vnstat htop screen mc
 
 username="nknrus"
-benaddress="ВАШ_КОШЕЛЕК"
-websource="https://nkn.org/ChainDB_pruned_latest.tar.gz"
-keys="ВАШ-ФАЙЛ_Wallet_PSWD_Wallet_JSON"
+benaddress="NKNWZs8cHhdsniNcjRgx3DFcNtjeWZDzc9si"
 
 useradd -m -p "pass" -s /bin/bash "$username" > /dev/null 2>&1
 usermod -a -G sudo "$username" > /dev/null 2>&1
 
-# NKNRUS.RU - Установка ноды
 printf "NKNRUS.RU - Загрузка ноды........................................... "
 cd /home/$username > /dev/null 2>&1
 wget --quiet --continue --show-progress https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip > /dev/null 2>&1
@@ -33,11 +30,7 @@ DIR="/home/$username/nkn-commercial/services/nkn-node/"
                         systemctl stop nkn-commercial.service > /dev/null 2>&1
 			sleep 20
                         cd $DIR > /dev/null 2>&1
-			rm wallet.json
-			rm wallet.pswd
                         rm -rf ChainDB/ > /dev/null 2>&1
-                        wget -O - "$keys" -q --show-progress | tar -xf -
-  			wget -O - "$websource" -q --show-progress | tar -xzf -
                         chown -R $username:$username wallet.* > /dev/null 2>&1
                         chown -R $username:$username ChainDB/ > /dev/null 2>&1
                         printf "NKNRUS.RU - Скачивание файла................. Выполнено!\n"
